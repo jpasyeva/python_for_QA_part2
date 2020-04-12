0d85a39458e52f9d024c13e89abb2201e7418dff
Julia <jp@utair.digital>
2020-04-12 15:25:21 +0300

    исправлена ошибка

--- tests/test_del_project.py
+++ tests/test_del_project.py
@@ -8,7 +8,7 @@ def test_del_some_project(app):
     if len(app.soap.get_project_list(username, password)) == 0:
         app.project.create(Project(name=app.project.random_name_field("project_", 4)))
     old_projects = app.soap.get_project_list()
-    project = random.choice(old_project)
+    project = random.choice(old_projects)
     app.project.del_project_by_id(project.id)
     new_projects = app.soap.get_project_list(username, password)
     assert len(old_projects) - 1 == len(new_projects)
