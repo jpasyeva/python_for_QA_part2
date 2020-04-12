0f9bd95d80fdd3aa1ddd7a1dc24958f512487074
Julia <jp@utair.digital>
2020-04-12 13:44:47 +0300

    Реализация использования ftp

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 import json
 import os.path
 from fixture.application import Application
+import ftputil
 
 fixture = None
 target = None
@@ -16,16 +17,47 @@ def load_config(file):
     return target
 
 
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
+
 @pytest.fixture
 def app(request):
     global fixture
     global target
     browser = request.config.getoption("--browser")
-    webconfig = load_config(request.config.getoption("--target"))['web']
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=webconfig["baseUrl"])
+        fixture = Application(browser=browser, base_url=config["web"]["baseUrl"])
     fixture.session.ensure_login("administrator", "root") #для тестов которым нужны админ права
     return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+
+    def fin():
+        restore_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    request.addfinalizer(fin)
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.back"):
+            remote.remove("config_inc.php.back")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc.php.back")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resourses/config_inc.php"), "config_inc.php")
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.back"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.back", "config_inc.php")
 
 
 @pytest.fixture(scope="session", autouse=True)
---
+++ resourses/config_inc.php
@@ -0,0 +1,8 @@
+<?php
+$g_hostname = 'localhost';
+$g_db_type = 'mysql';
+$g_database_name = 'mantis';
+$g_db_username = 'root';
+$g_db_password = '';
+$g_use_captcha = 'off';
+$g_signup_use_captcha = 'off';
